fb84270bb7b7e0d06af5f7ba0f2de5ee2ebf3d92
Steve Eisner <steveeisner@gmail.com>
2022-08-24 15:12:27 -0700

    Ability to set BPM from any node, using double click
    (cherry picked from commit 0ae721651f7bfa549946dea205ef0ec45ed79d50)

--- usermods/Tubes/global_state.h
+++ usermods/Tubes/global_state.h
@@ -58,5 +58,7 @@ typedef uint8_t CommandId;
 const static CommandId COMMAND_OPTIONS = 0x10;
 const static CommandId COMMAND_STATE = 0x20;
 const static CommandId COMMAND_ACTION = 0x30;
+const static CommandId COMMAND_INFO = 0x40;
+const static CommandId COMMAND_BEATS = 0x50;
 const static CommandId COMMAND_UPGRADE = 0xE0;
 const static CommandId COMMAND_RESET = 0xF0;
